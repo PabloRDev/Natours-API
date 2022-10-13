570b2de13a184cbf9e2c459f45a012ee8b7b7381
Pablo RDev <pabloriojod@gmail.com>
2022-10-13 17:09:02 +0200

    Fixing Heroku

--- app.js
+++ app.js
@@ -26,8 +26,7 @@ const limiterAPICalls = rateLimit({
 })
 
 app.use(helmet())
-process.env.NODE_ENV === 'development' &&
-  app.use(morgan('dev'))
+process.env.NODE_ENV === 'development' && app.use(morgan('dev'))
 app.use(express.json({ limit: '10kb' }))
 // app.use(express.static(`${__dirname}/public`))
 app.use(mongoSanitize())
