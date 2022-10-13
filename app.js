88379165e8a05f646f03902787df1ed4c0ba8b95
Pablo RDev <pabloriojod@gmail.com>
2022-10-13 16:27:53 +0200

    Compression package

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet')
 const mongoSanitize = require('express-mongo-sanitize')
 const xss = require('xss-clean')
 const hpp = require('hpp')
+const compression = require('compression')
 
 const globalErrorHandler = require('./controllers/errorController')
 
@@ -59,5 +60,6 @@ app.all('*', (req, res, next) => {
 })
 
 app.use(globalErrorHandler)
+app.use(compression())
 
 module.exports = app
